2ecccf4d1c97dc2f024a1c5872400357a49847d8
Tomas Moyano Santis <tomytool@gmail.com>
2024-11-30 15:42:48 -0300

    se vuelve a un inicio de los commit

--- src/Tablas.jsx
+++ src/Tablas.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
 import datos from './assets/tipificaciones.json';
-import styled from 'styled-components';
 import { AiFillCaretRight, AiFillCaretLeft } from 'react-icons/ai';
 import { responsableFiltro, servicioFiltro } from './assets/filtros.js';
 import {
@@ -15,8 +13,6 @@ import {
 export const Tablas = () => {
   const [dato, setDato] = useState([...datos]);
   const [filter, setFilter] = useState('');
-  // definicion del hook form
-  const { register, handleSubmit } = useForm();
 
   console.log(dato);
 
@@ -54,133 +50,45 @@ export const Tablas = () => {
     onGlobalFilterChange: setFilter,
   });
 
-  // funciones de filtrado denarchivo
-  const filtrado = handleSubmit((filtro) => {
-    const responsable = filtro.responsable;
-    const servicio = filtro.servicio;
-    if (responsable === 'TODOS' && servicio === 'TODOS') {
-      setDato([...datos]);
-      return;
-    }
-    setDato(
-      datos.filter(
-        (item) =>
-          (responsable === 'TODOS' || item.RESPONSABLE === responsable) &&
-          (servicio === 'TODOS' || item.SERVICIO === servicio)
-      )
-    );
-  });
-
-  // styled components para la tabla
-  const Formulario = styled.form`
-    display: flex;
-    flex-direction: column;
-    align-items: start;
-    padding: 15px;
-    row-gap: 10px;
-    background-color: #bfbfbf;
-    width: 280px;
-    border-radius: 6px;
-    margin-bottom: 20px;
-  `;
-
-  const Opciones = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: start;
-    row-gap: 10px;
-  `;
-
-  const Input = styled.input`
-    width: 272px;
-    height: 30px;
-    border-radius: 6px;
-  `;
-
-  const Select = styled.select`
-    padding: 6px 12px;
-    width: 280px;
-    border-radius: 6px;
-  `;
-
-  const Boton = styled.button`
-    background-color: #404040;
-    border: none;
-    color: white;
-    font-size: 16px;
-    padding: 6px 12px;
-    cursor: pointer;
-    border-radius: 6px;
-    &:hover {
-      background-color: #bfbfbf;
-      color: black;
-    }
-  `;
-
-  const Tabla = styled.table`
-    margin-bottom: 20px;
-    border-collapse: collapse;
-  `;
-
-  const CabeceraTabla = styled.thead`
-    background-color: #0d0d0d;
-    color: white;
-  `;
-  const CuerpoTabla = styled.tbody`
-    tr {
-      background-color: lightgray;
-      &:nth-child(odd) {
-        background-color: white;
-      }
-    }
-  `;
-
-  const GrupoBotones = styled.div`
-    display: flex;
-    width: 300px;
-    justify-content: space-between;
-    column-gap: 6px;
-  `;
-
   // renderizacion del componente
 
   return (
     <>
       <h2>Tipificaciones 2023</h2>
-      <Formulario onSubmit={filtrado} className="formulario">
-        <Opciones>
+      <form className="formulario">
+        <div className="opciones">
           <label htmlFor="responsable">Responsable</label>
-          <Select id="responsable" {...register('responsable')}>
+          <select id="responsable">
             {responsableFiltro.map((item) => (
               <option key={item} value={item}>
                 {item}
               </option>
             ))}
-          </Select>
-        </Opciones>
-        <Opciones>
+          </select>
+        </div>
+        <div className="opciones">
           <label htmlFor="servicio">Servicio</label>
-          <Select id="servicio" {...register('servicio')}>
+          <select id="servicio">
             {servicioFiltro.map((item) => (
               <option key={item} value={item}>
                 {item}
               </option>
             ))}
-          </Select>
-        </Opciones>
-        <Opciones>
+          </select>
+        </div>
+        <div className="opciones">
           <label htmlFor="filtrar">Filtrar</label>
-          <Input
+          <input
             id="filtrar"
             type="text"
             value={filter}
             onChange={(event) => setFilter(event.target.value)}
           />
-        </Opciones>
-        <Boton type="submit">Filtrar</Boton>
-      </Formulario>
-      <Tabla>
-        <CabeceraTabla>
+        </div>
+        <button type="submit">Filtrar</button>
+      </form>
+      <table>
+        <thead>
           {tabla.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
@@ -188,8 +96,8 @@ export const Tablas = () => {
               ))}
             </tr>
           ))}
-        </CabeceraTabla>
-        <CuerpoTabla>
+        </thead>
+        <tbody>
           {tabla.getRowModel().rows.map((row) => (
             <tr key={row.id}>
               {row.getVisibleCells().map((cell) => (
@@ -199,21 +107,21 @@ export const Tablas = () => {
               ))}
             </tr>
           ))}
-        </CuerpoTabla>
-      </Tabla>
-      <GrupoBotones>
-        <Boton className="boton" onClick={() => tabla.firstPage()}>
+        </tbody>
+      </table>
+      <div className="grupoBotones">
+        <button className="boton" onClick={() => tabla.firstPage()}>
           Primera pagina
-        </Boton>
-        <Boton
+        </button>
+        <button
           className="boton"
           onClick={() => {
             tabla.previousPage();
           }}
         >
           <AiFillCaretLeft />
-        </Boton>
-        <Boton
+        </button>
+        <button
           className="boton"
           onClick={() => {
             tabla.nextPage();
@@ -221,11 +129,11 @@ export const Tablas = () => {
           }}
         >
           <AiFillCaretRight />
-        </Boton>
-        <Boton className="boton" onClick={() => tabla.lastPage()}>
+        </button>
+        <button className="boton" onClick={() => tabla.lastPage()}>
           ultima pagina
-        </Boton>
-      </GrupoBotones>
+        </button>
+      </div>
     </>
   );
 };
